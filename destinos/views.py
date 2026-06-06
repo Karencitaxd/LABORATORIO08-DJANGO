4ebaad7eae1d3e11556d225a9b1ced50d398b704
Karencitaxd <kalvarezmo@unsa.edu.pe>
2026-06-05 21:51:27 -0500

    Implementacion de vistas dinamicas con tags for e if

--- destinos/views.py
+++ destinos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import DestinoTuristico
 
-# Create your views here.
+def index(request):
+    destinos = DestinoTuristico.objects.all()
+
+    return render(request, 'destinos/index.html', {
+        'destinos': destinos
+    })
